acf9671ded2899e7114f8bddae93f00b56cb16df
Robert <52195359+SeparateRecords@users.noreply.github.com>
2021-09-19 11:16:38 +1000

    Add entrypoint in repo root

---
+++ mod.ts
@@ -0,0 +1,5 @@
+// src/mod.ts is the canonical entrypoint for this module.
+// This file exists so you can import from GitHub without
+// having to specify the src/ directory.
+
+export * from "./src/mod.ts";
