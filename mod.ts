3bd988d09cf0c015fdaca429bf03ab4cd82ea6dc
Robert <52195359+SeparateRecords@users.noreply.github.com>
2021-09-19 11:17:19 +1000

    Revert "Add entrypoint in repo root"
    This reverts commit 32feac07913910ad150eccf6178cf7304de8ddbe.

--- mod.ts
+++
@@ -1,5 +0,0 @@
-// src/mod.ts is the canonical entrypoint for this module.
-// This file exists so you can import from GitHub without
-// having to specify the src/ directory.
-
-export * from "./src/mod.ts";
