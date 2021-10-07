a4c992637803df36a428124e597f66421bc70d4f
Robert <52195359+SeparateRecords@users.noreply.github.com>
2021-10-07 14:21:17 +1100

    Remove VPP property from GET /users/:id

--- src/schemas/GET_users_id.ts
+++ src/schemas/GET_users_id.ts
@@ -1,15 +1,52 @@
 import ajv, { JTDSchemaType } from "./_ajv_jtd.ts";
-import * as user from "./_user.ts";
 
 type ResponseData = {
 	code: number;
-	user: user.UserData;
+	user: {
+		id: number;
+		locationId: number;
+		status: "Active" | "Trash";
+		deviceCount: number;
+		email: string;
+		username: string;
+		domain: string;
+		firstName: string;
+		lastName: string;
+		name: string;
+		groupIds: number[];
+		groups: string[];
+		teacherGroups: number[];
+		children: number[];
+		notes: string;
+		exclude: boolean;
+		modified: string;
+	};
 };
 
 const responseSchema: JTDSchemaType<ResponseData> = {
 	properties: {
 		code: { type: "int32" },
-		user: user.userDataSchema,
+		user: {
+			properties: {
+				id: { type: "int32" },
+				locationId: { type: "int32" },
+				status: { enum: ["Active", "Trash"] },
+				deviceCount: { type: "int32" },
+				email: { type: "string" },
+				username: { type: "string" },
+				domain: { type: "string" },
+				firstName: { type: "string" },
+				lastName: { type: "string" },
+				name: { type: "string" },
+				groupIds: { elements: { type: "int32" } },
+				groups: { elements: { type: "string" } },
+				teacherGroups: { elements: { type: "int32" } },
+				children: { elements: { type: "int32" } },
+				notes: { type: "string" },
+				exclude: { type: "boolean" },
+				modified: { type: "string" },
+			},
+		},
 	},
 };
 
