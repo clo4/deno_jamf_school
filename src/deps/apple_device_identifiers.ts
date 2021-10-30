3e6a7f60198b23e46d981051b7567991bedbd636
Robert <52195359+SeparateRecords@users.noreply.github.com>
2021-10-30 15:11:40 +1100

    Update identifiers again

--- src/deps/apple_device_identifiers.ts
+++ src/deps/apple_device_identifiers.ts
@@ -1,1 +1,1 @@
-export * from "https://raw.githubusercontent.com/SeparateRecords/apple_device_identifiers/f2aed44fadf4a532d6141dd263a728d3bdb61ab7/mod.ts";
+export * from "https://raw.githubusercontent.com/SeparateRecords/apple_device_identifiers/76c548ca78a2307039741246c999547da383ae14/mod.ts";
