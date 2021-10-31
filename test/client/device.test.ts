cd5e64b3d5e3d14c0ca72bd1944cf15baac8f1e8
Robert <52195359+SeparateRecords@users.noreply.github.com>
2021-10-31 12:05:19 +1100

    Add test

--- test/client/device.test.ts
+++ test/client/device.test.ts
@@ -1,7 +1,7 @@
 import * as jamf from "../../src/mod.ts";
 import { assertValid } from "../../src/schemas/mod.ts";
 import { relativeTextFileReader } from "../deps/read_relative_file.ts";
-import { assertEquals } from "../deps/std_testing_asserts.ts";
+import { assert, assertEquals } from "../deps/std_testing_asserts.ts";
 
 const readRelativeTextFile = relativeTextFileReader(import.meta.url);
 
@@ -25,5 +25,18 @@ Deno.test({
   fn() {
     const device = client.createDevice(data.devices[0]);
     assertEquals(device.toString(), device.name);
+  },
+});
+
+Deno.test({
+  name: "Device.enrollment is an object with 'type' and 'pending'",
+  fn() {
+		for (const deviceData of data.devices) {
+			const device = client.createDevice(deviceData);
+			const obj = device.enrollment;
+			assert(typeof obj === "object");
+			assert("type" in obj);
+			assert("pending" in obj);
+		}
   },
 });
