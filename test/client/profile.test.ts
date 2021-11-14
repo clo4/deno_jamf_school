c88c6943a1988e4d62398adfb6cf53a475c9f4dd
Robert <52195359+SeparateRecords@users.noreply.github.com>
2021-11-14 15:34:00 +1100

    Test Profile objects

---
+++ test/client/profile.test.ts
@@ -0,0 +1,39 @@
+import * as jamf from "../../src/mod.ts";
+import { assertValid } from "../../src/schemas/mod.ts";
+import { relativeTextFileReader } from "../deps/read_relative_file.ts";
+import { assertEquals } from "../deps/std_testing_asserts.ts";
+
+const readRelativeTextFile = relativeTextFileReader(import.meta.url);
+
+const client = jamf.createClient({ id: "", token: "", url: "" });
+
+const data = JSON.parse(
+	await readRelativeTextFile("../example_data/GET_profiles__200.json"),
+);
+assertValid("GET /profiles", data);
+
+Deno.test({
+	name: "Profile.toJSON() is the same as the data used to create it",
+	fn() {
+		const profile = client.createProfile(data.profiles[0]);
+		assertEquals(profile.toJSON(), data.profiles[0]);
+	},
+});
+
+Deno.test({
+	name: "Profile.toString() is the same as Profile.name",
+	fn() {
+		const profile = client.createProfile(data.profiles[0]);
+		assertEquals(profile.toJSON(), data.profiles[0]);
+	},
+});
+
+Deno.test({
+	name: "Calling Profile.timeConstraints() doesn't fail (with any of the test data)",
+	fn() {
+		const profiles = data.profiles.map(profile => client.createProfile(profile));
+		for (const profile of profiles) {
+			profile.timeConstraints();
+		}
+	}
+})
