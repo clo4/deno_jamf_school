ce08ea5e77deb96898476f43e9ab7c05d3f09e44
Robert <52195359+SeparateRecords@users.noreply.github.com>
2021-11-22 11:52:22 +1100

    remove duplicates

--- src/full.ts
+++ src/full.ts
@@ -6,5 +6,30 @@ export function createClientWith(api: API): Client {
 	return new InternalClient({ api });
 }
 
-export * from "./api.ts";
-export * from "./mod.ts";
+export type {
+	API,
+	APIDeviceData,
+	APIDeviceGroupData,
+	APIGetDeviceOptions,
+	APIGetDevicesOptions,
+	APIMoveDeviceOptions,
+	APIMoveDevicesOptions,
+	APIMoveUserOptions,
+	APIRestartDeviceOptions,
+	APIUserData,
+	APIUserGroupData,
+	APIWipeDeviceOptions,
+	App,
+	Client,
+	Credentials,
+	Device,
+	DeviceGroup,
+	Location,
+	User,
+	UserGroup,
+} from "./models/mod.ts";
+
+export { version } from "./version.ts";
+
+export { createAPI } from "./api.ts";
+export { createClient } from "./mod.ts";
