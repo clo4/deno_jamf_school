42da2977c713eebd8e76f0319e6778ca3f0b5265
Robert <52195359+SeparateRecords@users.noreply.github.com>
2021-11-23 13:44:40 +1100

    Testing...

--- src/mod.ts
+++ src/mod.ts
@@ -1,11 +1,27 @@
 import type { Client, Credentials } from "./models/mod.ts";
 
 import { Client as InternalClient } from "./internal/Client.ts";
-import { createAPI } from "./api.ts";
+import * as api from "./api.ts";
 
-export function createClient(credentials: Credentials): Client {
-	const api = createAPI(credentials);
-	return new InternalClient({ api });
+/**
+ * Create an API client.
+ *
+ * Clients are an object-oriented wrapper over the Jamf School API.
+ *
+ * ```
+ * const client = jamf.createClient({
+ *   id: "your_network_id",
+ *   token: "your_api_token",
+ *   url: "https://your_school.jamfcloud.com",
+ * });
+ * ```
+ */
+export function createClient(init: Credentials | { api: api.API }): Client {
+	return new InternalClient({
+		api: "api" in init //
+			? init.api
+			: api.createAPI(init),
+	});
 }
 
 export type {
