eb7f8ba8e8c8ee5d2ccca02280e1e959d2804590
Robert <52195359+SeparateRecords@users.noreply.github.com>
2021-11-30 18:51:56 +1100

    Rename example, use cliffy

--- src/examples/restart_every_morning.ts
+++ src/examples/restart_every_morning.ts
@@ -1,20 +1,17 @@
 import { cron } from "https://deno.land/x/deno_cron@v1.0.0/cron.ts";
+import { List } from "https://deno.land/x/cliffy@v0.20.1/prompt/list.ts";
 
 import { api } from "./auth.ts";
 
-const udids = [
-	"Put some UDIDs (unique device identifiers) in this array!",
-	"When viewing a device on the Jamf School website, its UDID is in the URL.",
-];
+const udids = await List.prompt("Enter some UDIDs");
 
 // Visit https://crontab.guru to create your own schedule.
 // "At the 0th minute of the 7th hour (on every day, month, and day-name)"
 cron("0 7 * * *", () => restart());
 
 async function restart() {
-	return await Promise.allSettled(udids.map((udid) =>
-		api.restartDevice(udid, {
-			clearPasscode: true,
-		})
-	));
+	// 📍 Using the API method to restart devices (in parallel)
+	return await Promise.allSettled(
+		udids.map((udid) => api.restartDevice(udid, { clearPasscode: true })),
+	);
 }
